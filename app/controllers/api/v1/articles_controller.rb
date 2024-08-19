c87cdd955c3e9928087d0f6257a1a099f89cc95a
adhamnaiji <adhemnaiji@gmail.com>
2024-08-19 09:05:31 -0400

    auth_dev

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,51 @@
+
+
+  # app\controllers\api\v1\articles_controller.rb
+class Api::V1::ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :update, :destroy]
+
+  def index
+    @articles = Article.all
+    render json: @articles
+  end
+
+  def show
+    render json: @article
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  # Devise routes for user authentication
   devise_for :users
-root "articles#index"
- resources :articles do
+
+  # Root path
+  #root "api/v1/articles#index"
+ 
+  root "articles#index"
+
+
+  # Articles and comments routes
+  resources :articles do
     resources :comments
   end
-end
 
+  # Namespaced API routes
+  namespace :api do
+    namespace :v1 do
+      resources :articles
+    end
+  end
+end
